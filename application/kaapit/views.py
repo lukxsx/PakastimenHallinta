41f006e021f6aa6ee42899c6d2937508292b4516
Lukxsx <luks@luks.moe>
2020-03-16 19:20:35 +0200

    Kaappien lisääminen

--- application/kaapit/views.py
+++ application/kaapit/views.py
@@ -1,17 +1,21 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.kaapit.models import Kaappi
 
+@app.route("/kaapit", methods=["GET"])
+def kaappi_listaus():
+    return render_template("kaapit/listaa.html", kaapit = Kaappi.query.all())
+
 @app.route("/kaapit/lisaa/")
-def kaappi_kysely():
+def kaappi_lisays():
     return render_template("kaapit/lisaa.html")
 
 @app.route("/kaapit/", methods=["POST"])
-def kaappi_lisays():
+def kaappi_lisaaja():
     nimi = request.form.get("nimi")
     tasot = request.form.get("tasot")
     k = Kaappi(nimi, tasot)
     db.session().add(k)
     db.session().commit()
   
-    return "hello world!"
+    return redirect(url_for("kaappi_listaus"))
