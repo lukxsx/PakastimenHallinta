073a80ae48be5a36c72882076931bc552ff2a022
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-04-30 22:38:08 +0300

    ulkoasun parantelua

--- application/elintarvike/views.py
+++ application/elintarvike/views.py
@@ -1,21 +1,24 @@
-from application import app, db
 from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
+
+from application import app, db
+from application.elintarvike.forms import ElintarvikeForm, PaivitysForm
 from application.elintarvike.models import Elintarvike
 from application.elintarvikekaapissa.models import ElintarvikeKaapissa
-from application.elintarvike.forms import ElintarvikeForm, PaivitysForm
 
 
 @app.route("/elintarvikkeet", methods=["GET"])
 @login_required
 def elintarvike_listaus():
-    return render_template("elintarvikkeet/listaa.html", elintarvikkeet=Elintarvike.query.filter_by(kayttaja_id=current_user.id).all(), pform=PaivitysForm())
+    return render_template("elintarvikkeet/listaa.html",
+                           elintarvikkeet=Elintarvike.query.filter_by(kayttaja_id=current_user.id).all(),
+                           pform=PaivitysForm())
 
 
 @app.route("/elintarvikkeet/lisaa/")
 @login_required
 def elintarvike_lisays():
-    return render_template("elintarvikkeet/lisaa.html", form = ElintarvikeForm())
+    return render_template("elintarvikkeet/lisaa.html", form=ElintarvikeForm())
 
 
 @app.route("/elintarvikkeet/<elintarvike_id>/", methods=["POST"])
@@ -26,6 +29,7 @@ def elintarvike_paivitys(elintarvike_id):
     db.session.commit()
     return redirect(url_for("elintarvike_listaus"))
 
+
 @app.route("/elintarvikkeet/poista/<elintarvike_id>/", methods=["POST"])
 @login_required
 def elintarvike_poisto(elintarvike_id):
@@ -35,15 +39,17 @@ def elintarvike_poisto(elintarvike_id):
         db.session.delete(elin)
         db.session.commit()
     else:
-        return render_template("error.html", errormessage="VIRHE: Elintarviketyyppiä, joka on kaapissa ei voida poistaa.")
+        return render_template("error.html",
+                               errormessage="VIRHE: Elintarviketyyppiä, joka on kaapissa ei voida poistaa.")
     return redirect(url_for("elintarvike_listaus"))
+
 
 @app.route("/elintarvikkeet/", methods=["POST"])
 @login_required
 def elintarvike_lisaaja():
     form = ElintarvikeForm(request.form)
     if not form.validate():
-        return render_template("elintarvikkeet/lisaa.html", form = form)
+        return render_template("elintarvikkeet/lisaa.html", form=form)
 
     e = Elintarvike(form.nimi.data, form.sailyvyys.data)
     e.kayttaja_id = current_user.id
